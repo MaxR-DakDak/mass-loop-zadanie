40db6a692ece92fb2634832f9b3c34c3729c0f4b
maxro <maxrozko@gmail.com>
2022-03-13 11:35:19 +0100

    mobile start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,340 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Menu/>
+   <div class="page">
+      <!-- header -->
+      <div class="page__header title title_big">Inteligentna kruszarka, która zamienia szkło w piasek.</div>
+      <img class="page__img" src="@/assets/maasloop.png" alt="maasloop">
+      <img class="page__mob-btn" src="@/assets/mob_btn.png" alt="addtocart">
+
+      <!-- rectangle -->
+      <div class="page__rectangle">
+         <div class="title">Płać 5 razy mniej za wywóz odpadów szklanych.</div>
+         <div class="text">Kruszarka zmniejsza gabaryty szklanych śmieci, a Ty płacisz PIĘ-CIO-KROT-NIE niższe rachunki za ich wywóz.</div>
+      </div>
+
+      <!-- calculator -->
+      <div class="page__calculator-title title">Ile zaoszczędzisz?</div>
+      <div class="page__calculator-subtitle text">Rachunek jest prosty.</div>
+      <div class="page__calculator">
+         <div class="page__input-blocks">
+            <div class="page__input-block">
+               <div class="title title_line20 title_extra-small">Ile pojemników na szkło zapełniasz miesięcznie?</div>
+               <input class="page__input text" type="text" name="" id="">
+            </div>
+            <div class="page__input-block page__input-block_small">
+               <div class="title title_line20 title_extra-small">Ile płacisz za wywóz jednego pojemnika na szkło?</div>
+               <input class="page__input text" type="text" name="" id="">
+            </div>
+         </div>
+         <div class="page__result">
+            <div class="title title_white title_extra-small"> Dzięki kruszarce zaoszczędzisz 900 zł rocznie.</div>
+         </div>
+      </div>
+
+      <!-- bottom -->
+      <div class="page__bottom_text text">To na co wydasz zaoszczędzoną gotówkę?</div>
+      <div class="page__btn">KUP<span class="page__btn_price">8499 zł</span></div>
+      <div class="page__bottom_title title title_small"> <span class="page__bottom-margin"> Brzmi dobrze? </span> Uważaj. Dopiero się rozkręcamy!</div>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+   name: 'App',
+   components: {
+      Menu,
+   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap');
+
+$text_color_21: #212121;
+
+body {
+   font-family: 'Montserrat', sans-serif;
+   margin: 0;
+}
+
+.page {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   padding-top: 90px;
+   padding-bottom: 90px;
+   overflow-x: hidden;
+   text-align: center;
+
+   @media screen and (max-width: 575.98px) {
+      padding-left: 20px;
+      padding-right: 20px;
+   }
+
+   &__header {
+      max-width: 997px;
+      margin-top: 74px;
+      padding-left: 1px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 45px;
+      }
+   }
+
+   &__img {
+      margin-top: 59px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 57px;
+         width: 142%;
+         height: auto;
+      }
+   }
+
+   &__mob-btn{
+      display: none;
+
+      @media screen and (max-width: 575.98px) {
+         display: block;
+         position: absolute;
+         right: 0;
+         bottom: -4px;
+      }
+
+   }
+
+   &__rectangle {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 100%;
+      height: 365px;
+      box-sizing: border-box;
+      background-color: #FFB001;
+      border: 20px solid white;
+      border-radius: 30px;
+      margin-top: -400px;
+
+      @media screen and (max-width: 575.98px) {
+         padding: 50px 20px 74px;
+         border-width: 15px;
+         margin-top: -182px;
+         height: 343px;
+         width: calc(100% + 70px);
+      }
+   }
+
+   &__calculator-title {
+      margin-top: 91px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 48px;
+      }
+   }
+
+   &__calculator-subtitle {
+      margin-top: 40px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 28px;
+      }
+   }
+
+   &__calculator {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      gap: 20px;
+      width: 100%;
+      max-width: 1012px;
+      margin-top: 84px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 60px;
+      }
+
+      &_block {
+         display: inline-flex;
+         gap: 30px;
+      }
+   }
+
+   &__input-blocks {
+      display: inline-flex;
+      gap: 30px;
+
+      @media screen and (max-width: 575.98px) {
+         flex-wrap: wrap;
+         gap: 20px
+      }
+   }
+
+   &__input-block {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+      flex-grow: 1;
+      box-sizing: border-box;
+      border-radius: 15px;
+      background-color: #fafafd;
+      height: 260px;
+
+      @media screen and (max-width: 575.98px) {
+         height: 240px;
+         padding: 41px 34px 40px 34px
+      }
+
+      &_small {
+         @media screen and (max-width: 575.98px) {
+            height: 230px;
+         }
+      }
+   }
+
+   &__input {
+      height: 66px;
+      max-width: 219px;
+      border-radius: 15px;
+      border: 1px solid #{$text_color_21};
+      text-align: center;
+      outline: none;
+
+      @media screen and (max-width: 575.98px) {
+         width: 225px;
+      }
+
+      &:active, &:focus {
+         border-color: #FFB001;
+      }
+   }
+
+   &__result {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 127px;
+      box-sizing: border-box;
+      background-color: #{$text_color_21};
+      border-radius: 15px;
+
+      @media screen and (max-width: 575.98px) {
+         height: 213px;
+      }
+   }
+
+   &__bottom_text {
+      margin-top: 82px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 60px;
+      }
+   }
+
+   &__btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 7px;
+      box-sizing: border-box;
+      border-radius: 47.5px;
+      background-color: #FFB001;
+      height: 95px;
+      width: 386px;
+      margin-top: 59px;
+      font-weight: 900;
+      font-size: 24px;
+      cursor: pointer;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 62px;
+         width: 100%;
+         height: 80px;
+         line-height: 24px;
+         font-size: 20px;
+      }
+
+      &_price {
+         font-weight: 400;
+      }
+   }
+
+   &__bottom_title {
+      margin-top: 78px;
+
+      @media screen and (max-width: 575.98px) {
+         margin-top: 60px;
+         width: 243px;
+         margin-left: 1px;
+      }
+   }
+
+   &__bottom-margin{
+      margin-left: -3px;
+   }
+}
+
+.title {
+   font-size: 40px;
+   font-weight: 900;
+   color: #{$text_color_21};
+
+   @media screen and (max-width: 575.98px) {
+      font-size: 24px;
+   }
+
+   &_big {
+      font-size: 55px;
+      @media screen and (max-width: 575.98px) {
+         font-size: 35px;
+      }
+   }
+
+   &_small {
+      font-size: 28px;
+
+      @media screen and (max-width: 575.98px) {
+         font-size: 20px;
+      }
+   }
+
+   &_extra-small {
+      font-size: 20px;
+      @media screen and (max-width: 575.98px) {
+         font-size: 16px;
+      }
+   }
+
+   &_line20 {
+      line-height: 20px
+   }
+
+   &_white {
+      color: white;
+   }
+}
+
+.text {
+   font-size: 20px;
+   font-weight: 400;
+   color: #{$text_color_21};
+   line-height: 1.59;
+
+   @media screen and (max-width: 575.98px) {
+      font-size: 16px;
+   }
+
+   &_900 {
+      font-weight: 900;
+   }
+
+   &_white {
+      color: white;
+   }
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,307 @@
+<template>
+   <div class="menu">
+      <div class="menu__content">
+         <img class="menu__logo" src="@/assets/logo.svg" alt="logo">
+
+         <div class="menu__links">
+            <a class="menu__link" href="/calc">Policz zyski</a>
+            <a class="menu__link" href="/products">O produkcie</a>
+            <a class="menu__link" href="/contact">Kontakt</a>
+            <a class="menu__link menu__link_faq" href="/faq">FAQ</a>
+         </div>
+
+         <div class="menu__btns">
+            <div class="menu__lang">
+               <span> PL </span>
+               <img class="menu_arrow" src="@/assets/arrow.svg" alt="lang">
+            </div>
+
+            <div class="menu__buynow menu__buynow_desktop">
+               KUP
+            </div>
+
+            <div @click="hamburgerAnim($event)" class="menu__hamburger">
+               <div class="menu__hamburger-line"></div>
+               <div class="menu__hamburger-line"></div>
+               <div class="menu__hamburger-line"></div>
+            </div>
+
+            <div v-bind:class="{ active: showMobList }" class="menu__links_mobile">
+               <a class="menu__link menu__link_mobile1" href="/calc">Policz zyski</a>
+               <a class="menu__link menu__link_mobile2" href="/products">O produkcie</a>
+               <a class="menu__link menu__link_mobile3" href="/contact">Kontakt</a>
+               <a class="menu__link menu__link_mobile4" href="/faq">FAQ</a>
+
+               <div class="menu__buynow menu__buynow_mobile">
+                  KUP
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'PageMenu',
+   data() {
+      return {
+         showMobList: false,
+      }
+   },
+   methods: {
+      hamburgerAnim(event) {
+         event.currentTarget.classList.toggle('active');
+         this.showMobList = !this.showMobList;
+      }
+   }
+}
+</script>
+
+<!--scoped-->
+<style scoped lang="scss">
+
+$text_color: #212121;
+
+@mixin keyframes($animation-name) {
+   @-webkit-keyframes #{$animation-name} {
+      @content;
+   }
+   @-moz-keyframes #{$animation-name} {
+      @content;
+   }
+   @-ms-keyframes #{$animation-name} {
+      @content;
+   }
+   @-o-keyframes #{$animation-name} {
+      @content;
+   }
+   @keyframes #{$animation-name} {
+      @content;
+   }
+}
+
+@mixin animation($str) {
+   -webkit-animation: #{$str};
+   -moz-animation: #{$str};
+   -ms-animation: #{$str};
+   -o-animation: #{$str};
+   animation: #{$str};
+}
+
+@include keyframes(slideDown) {
+   0% {
+      height: 0;
+   }
+   100% {
+      height: 100vh;
+   }
+}
+
+
+.menu {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   position: fixed;
+   height: 90px;
+   left: 0;
+   top: 0;
+   right: 0;
+   z-index: 10;
+   background: #FFFFFF;
+   box-shadow: 0 4px 24px -5px rgba(0, 0, 0, 0.15);
+   padding-left: 110px;
+   padding-right: 110px;
+   color: #{$text_color};
+
+   @media screen and (max-width: 575.98px) {
+      padding-left: 20px;
+      padding-right: 20px;
+   }
+
+   &__logo {
+      height: 51px;
+      width: auto;
+
+      @media screen and (max-width: 575.98px) {
+         height: 41.38px;
+         width: auto;
+      }
+   }
+
+   &__content {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 57px;
+      width: 100%;
+   }
+
+   &__links {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 64px;
+      margin-left: 83px;
+
+      @media screen and (max-width: 575.98px) {
+         display: none;
+      }
+
+      &_mobile {
+         display: none;
+         position: fixed;
+         top: 0;
+         left: 0;
+         right: 0;
+         z-index: -1;
+         background-color: white;
+
+         @media screen and (max-width: 575.98px) {
+            &.active {
+               display: flex;
+               flex-direction: column;
+               align-items: center;
+               gap: 77px;
+               height: 100vh;
+               padding-top: 137px;
+               @include animation('slideDown 0.5s');
+            }
+         }
+      }
+   }
+
+   &__link {
+      text-decoration: none;
+      font-size: 16px;
+      font-weight: 400;
+      color: #{$text_color};
+
+      @media screen and (max-width: 575.98px) {
+         font-size: 15px;
+         font-weight: 500;
+         line-height: 18px;
+      }
+
+      &_faq {
+         margin-left: -6px;
+      }
+
+      @media screen and (max-width: 575.98px) {
+         &_mobile1 {
+            margin-left: -22px;
+         }
+         &_mobile2 {
+            margin-left: -23px;
+         }
+         &_mobile3 {
+            margin-left: -15px;
+         }
+         &_mobile4 {
+            margin-left: -5px;
+         }
+      }
+   }
+
+   &__btns {
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      gap: 26px;
+
+      @media screen and (max-width: 575.98px) {
+         gap: 29px;
+      }
+   }
+
+   &__lang {
+      display: flex;
+      gap: 10px;
+   }
+
+   &_arrow {
+      padding-top: 2px;
+   }
+
+   &__buynow {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #FFB001;
+      width: 152px;
+      height: 57px;
+      border: 2px solid #FFB001;
+      box-sizing: border-box;
+      box-shadow: 0 20px 59px -10px rgba(0, 0, 0, 0.15);
+      border-radius: 28.5px;
+      font-weight: 900;
+      font-size: 16px;
+      line-height: 20px;
+      padding-top: 2px;
+      cursor: pointer;
+
+      &_desktop{
+         @media screen and (max-width: 575.98px) {
+            display: none;
+         }
+      }
+
+      &_mobile{
+         height: 50px;
+      }
+   }
+
+   &__hamburger {
+      cursor: pointer;
+
+      &.active {
+         -webkit-transition: all 0.2s ease-in-out;
+         -o-transition: all 0.2s ease-in-out;
+         transition: all 0.2s ease-in-out;
+         -webkit-transition-delay: 0.4s;
+         -o-transition-delay: 0.4s;
+         transition-delay: 0.4s;
+         -webkit-transform: rotate(45deg);
+         -ms-transform: rotate(45deg);
+         -o-transform: rotate(45deg);
+         transform: rotate(45deg);
+      }
+
+      &.active div:nth-child(1) {
+         -webkit-transform: translateY(13px);
+         -ms-transform: translateY(13px);
+         -o-transform: translateY(13px);
+         transform: translateY(13px);
+         -webkit-transition-delay: 0.2s;
+         -o-transition-delay: 0.2s;
+         transition-delay: 0.2s;
+      }
+
+      &.active div:nth-child(2) {
+         width: 0;
+      }
+
+      &.active div:nth-child(3) {
+         -webkit-transform: translateY(-11px) rotate(90deg);
+         -ms-transform: translateY(-11px) rotate(90deg);
+         -o-transform: translateY(-11px) rotate(90deg);
+         transform: translateY(-11px) rotate(90deg);
+         -webkit-transition-delay: 0.2s;
+         -o-transition-delay: 0.2s;
+         transition-delay: 0.2s;
+      }
+   }
+
+   &__hamburger-line {
+      width: 36px;
+      height: 2px;
+      background: #{$text_color};
+      margin: 10px auto;
+      -webkit-transition: all 0.2s ease-in-out;
+      -o-transition: all 0.2s ease-in-out;
+      transition: all 0.2s ease-in-out;
+   }
+}
+
+</style>
